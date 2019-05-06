5f761723a68c6ab3ac2b00cb24edfa0c217fd137
Devynn Moreno <devynn.moreno@gmail.com>
2019-05-06 11:24:54 -0500

    test

--- app.py
+++ app.py
@@ -1,33 +1,3 @@
-# from flask import Flask, render_template, redirect
-
-# from flask_pymongo import PyMongo
-# import scrape_mars
-
-# app = Flask(__name__)
-
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/scrape_mars"
-# mongo = PyMongo(app)
-
-
-# @app.route('/')
-# def home():
-   
-#     all_data = mongo.db.mars_data_from_scrape.find_one()
-#     return render_template("index.html", all_data=all_data)
-
-   
-
-# @app.route("/scrape")
-# def scraper():
-#     all_data = mongo.db.all_data
-#     scrape_mars_data = scrape_mars.scrape()  
-#     all_data.update({}, scrape_mars_data, upsert=True)
-
-#     return redirect('/', code=302)
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
@@ -35,8 +5,10 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/scrape_mars"
+app.config["MONGO_URI"] = "mongodb://<dbuser>:<dbpassword>@ds145146.mlab.com:45146/heroku_81hbmzd3"
 mongo = PyMongo(app)
+
+
 
 
 @app.route("/")
